eb31f504d7ac1a9f344aa87d661ce2a26ed38e52
Sharon Leah <sharonleah@Sharons-MacBook-Air.local>
2025-09-05 01:15:24 +0300

    bottom nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:reps_trainer_app/utils/colors.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+     
       home: BottomNavScreen(),
+      getPages: [
+        // other screens navigation
+      ],
     );
   }
 }
 
 class BottomNavController extends GetxController {
   var selectedIndex = 0.obs;
+
 
   void changeIndex(int index) {
     selectedIndex.value = index;
@@ -24,45 +32,54 @@ class BottomNavController extends GetxController {
 
 class BottomNavScreen extends StatelessWidget {
   final List<Widget> _pages = [
-    Center(child: Text('Home')),
-    Center(child: Text('Stats')),
-    Center(child: Text('Tasks')),
-    Center(child: Text('Profile')),
-    Center(child: Text('Settings')),
+    Center(child: Text('Dashboard', style: TextStyle(color: textColor),)),
+    Center(child: Text('Stats', style: TextStyle(color: textColor),)),
+    Center(child: Text('Plans', style: TextStyle(color: textColor),)),
+    Center(child: Text('Players', style: TextStyle(color: textColor),)),
+    Center(child: Text('Profile', style: TextStyle(color: textColor),)),
   ];
+
+   BottomNavScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     final BottomNavController controller = Get.put(BottomNavController());
 
     return Scaffold(
+      backgroundColor: backgroundColor,
       body: Obx(() => _pages[controller.selectedIndex.value]),
       bottomNavigationBar: Obx(
         () => BottomNavigationBar(
+          backgroundColor: backgroundColor,
           currentIndex: controller.selectedIndex.value,
           onTap: controller.changeIndex,
-          selectedItemColor: Colors.purple,
-          unselectedItemColor: Colors.grey,
+          selectedItemColor: primaryColor,
+          unselectedItemColor: textColor,
           items: [
             BottomNavigationBarItem(
-              icon: Icon(Icons.flash_on),
-              label: 'Home',
+              icon: controller.selectedIndex.value == 0?Image.asset('assets/icons/dashboard_s.png'):Image.asset('assets/icons/dashboard.png'),
+              label: '',
+              backgroundColor: backgroundColor
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.bar_chart),
-              label: 'Stats',
+              icon: controller.selectedIndex.value == 1?Image.asset('assets/icons/stats_s.png'):Image.asset('assets/icons/stats.png'),
+              label: '',
+              backgroundColor: backgroundColor
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.checklist),
-              label: 'Tasks',
+              icon: controller.selectedIndex.value == 2? Image.asset('assets/icons/plans_s.png'): Image.asset('assets/icons/plans.png'),
+              label: '',
+              backgroundColor: backgroundColor
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: 'Profile',
+              icon: controller.selectedIndex.value == 3?Image.asset('assets/icons/players_s.png'):Image.asset('assets/icons/players.png'),
+              label: '',
+              backgroundColor: backgroundColor
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.settings),
-              label: 'Settings',
+              icon: controller.selectedIndex.value == 4?Image.asset('assets/icons/profile_s.png'):Image.asset('assets/icons/profile.png'),
+              label: '',
+              backgroundColor: backgroundColor
             ),
           ],
         ),
--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -2,19 +2,11 @@
 import 'package:flutter/material.dart';
 
 const appBarColor = Colors.white;
-const primaryColor = Color(0xFF051551);
+const primaryColor = Color(0xFF673ABE);
 const secondaryColor = Color(0xFF3858F0);
-const backgroundColor = Colors.white;
-const headingColor = Colors.black;
-const buttonTextColor = Colors.white;
-const buttonColor = Colors.black;
-const borderColor = Colors.grey;
-const tertiaryColor = Color(0xFFB9C6FB);
-const purpleColor = Color(0xFFAA0DE5);
-const errorColor = Colors.red;
-const containerColor1 = Color(0xFFAB0EE5);
-const fadeBlue = Color(0xFF90A1F1);
-const textColor = Colors.black;
+const backgroundColor = Color(0xFF0A0B0B);
+const textColor = Color(0xFFFFFFFF);
+
 
 
 
